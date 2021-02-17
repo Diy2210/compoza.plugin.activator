11090ab3a6255f267ce80bc7d7898cbe26a214fd
admin <d30jso39>
2021-02-17 17:45:20 +0200

    add http; add ActivatorApi class;

---
+++ lib/api/ActivatorApi.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart' as http;
+
+class ActivatorApi {
+  var client = http.Client();
+
+  getPluginList(String url, String token) {
+    //..."${server.url}/wp-json/deactivator/v1/list?token=${server.token}"
+    // try {
+    //   client.get('$url/wp-json/deactivator/v1/list?token=$token');
+    // } finally {
+    // client.close();
+    // }
+
+    //#1
+    // client.get('$url/wp-json/deactivator/v1/list?token=$token').then((response) {
+    //   print("Response body: ${response.body}");
+    // }).catchError((error) {
+    //   print("Error: $error");
+    // });
+
+    // #2
+    // client.get('https://json.flutter.su/echo').then((response) {
+    //   // print("Response status: ${response.statusCode}");
+    //   // print("Response body: ${response.body}");
+    // }).catchError((error){
+    //   print("Error: $error");
+    // });
+
+    //#3
+    try {
+      client.get('$url/wp-json/deactivator/v1/list?token=$token');
+    } catch(error) {
+      print(error);
+    }
+
+  updatePluginStatus(String token, int id, bool status) {
+    try {
+      // client.post(url)
+    } catch(error) {
+      print(error);
+    }
+}
